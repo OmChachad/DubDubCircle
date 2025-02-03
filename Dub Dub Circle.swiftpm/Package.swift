afefe1ea0bc553376fca20726d17159496becf2b
Om Chachad <omchachad12@gmail.com>
2025-02-03 15:48:07 +0530

    Updated Minimum Target Version to 18.0

--- Dub Dub Circle.swiftpm/Package.swift
+++ Dub Dub Circle.swiftpm/Package.swift
@@ -10,7 +10,7 @@ import AppleProductTypes
 let package = Package(
     name: "Dub Dub Circle",
     platforms: [
-        .iOS("17.0")
+        .iOS("18.0")
     ],
     products: [
         .iOSApplication(
