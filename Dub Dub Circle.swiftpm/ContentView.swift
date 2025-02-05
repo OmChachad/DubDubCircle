022c65bd6521d86fcddcd62fedc8da8d9e6dc022
Om Chachad <omchachad12@gmail.com>
2025-02-05 14:44:34 +0530

    Implemented Basic Home Page

--- Dub Dub Circle.swiftpm/ContentView.swift
+++ Dub Dub Circle.swiftpm/ContentView.swift
@@ -1,12 +1,53 @@
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query var events: [DeveloperEvent]
+    
+    @State private var columnVisibility: NavigationSplitViewVisibility = .doubleColumn
+    
+    @State private var showEventCreationSheet = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationSplitView(columnVisibility: $columnVisibility) {
+            Group {
+                if events.isEmpty {
+                    ContentUnavailableView("Your circle looks uneventful.", systemImage: "circle.dotted.circle", description: Text("Click \(Image(systemName: "plus.circle.fill")) to add your first event."))
+                        .frame(maxHeight: .infinity)
+                } else {
+                    List {
+                        ForEach(events) { event in
+                            NavigationLink {
+                                EventDetails(event: event)
+                            } label: {
+                                EventListItem(event: event)
+                            }
+                        }
+                    }
+                    .listStyle(InsetGroupedListStyle())
+                }
+            }
+            .navigationTitle("Dub Dub Circle")
+            .animation(.default, value: events.count)
+            .safeAreaInset(edge: .bottom) {
+                Button {
+                    showEventCreationSheet = true
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.title3.bold())
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.accentColor)
+                        .clipShape(Circle())
+                        .glow()
+                }
+            }
+            .sheet(isPresented: $showEventCreationSheet) {
+                NewEventView()
+            }
+        } detail: {
+            
         }
+
     }
 }
---
+++ Dub Dub Circle.swiftpm/EventListItem.swift
@@ -0,0 +1,74 @@
+//
+//  EventListItem.swift
+//  Dub Dub Circle
+//
+//  Created by Om Chachad on 2/5/25.
+//
+
+import SwiftUI
+
+struct EventListItem: View {
+    var event: DeveloperEvent
+    
+    var body: some View {
+        HStack {
+            
+            eventIconCircle()
+            
+            VStack(alignment: .leading) {
+                
+                Group {
+                    Text(event.title)
+                        .font(.headline)
+                    //                +
+                    Text((event.location?.name ?? "Online"))
+                        .foregroundColor(.primary.opacity(0.5))
+                }
+                .lineLimit(1)
+                
+                Text(event.date, style: .date)
+                    .font(.subheadline)
+                    .foregroundColor(.primary.opacity(0.5))
+                
+            }
+            
+            Spacer()
+        }
+    }
+    
+    func eventIconCircle() -> some View {
+        ZStack {
+            let attendees = event.attendees.prefix(3)
+            ForEach(attendees, id: \.self) { attendee in
+                let index = attendees.firstIndex(of: attendee)!
+                
+                attendee.profilePhoto
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width: 30, height: 30)
+                    .zIndex(Double(3 - index))
+                    .offset(x: CGFloat(index * 10))
+            }
+            
+            if attendees.count < 3 {
+                ForEach((attendees.count)..<3, id: \.self) { index in
+                    Circle()
+                        .fill(Gradient(colors: [.cyan, .purple]))
+                        .stroke(Color.white, lineWidth: 1)
+                        .frame(width: 30, height: 30)
+                        .zIndex(Double(3 - index))
+                        .offset(x: CGFloat(index * 10))
+                }
+            }
+        }
+        .offset(x: -10)
+        .frame(width: 50)
+        
+    }
+}
+
+#Preview {
+    let event = DeveloperEvent(title: "WWDC", date: Date(), wasOnline: true)
+    EventListItem(event: event)
+}
