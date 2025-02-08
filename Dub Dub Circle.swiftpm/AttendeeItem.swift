cec2798f1f1a23f03eb8a455dc81f366fa193293
Om Chachad <omchachad12@gmail.com>
2025-02-08 20:31:21 +0530

    Added Context Menu with Delete, Contact Options

--- Dub Dub Circle.swiftpm/AttendeeItem.swift
+++ Dub Dub Circle.swiftpm/AttendeeItem.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct AttendeeItem: View {
+    @Environment(\.openURL) var openURL
+    @Environment(\.modelContext) var modelContext
+    
     var attendee: Contact
     @Namespace var namespace: Namespace.ID
+    
+    @State private var showDeleteConfirmation = false
+    
     var body: some View {
         NavigationLink {
             AttendeeDetails(attendee: attendee)
@@ -21,6 +27,39 @@ struct AttendeeItem: View {
                 .scaledToFill()
                 .clipShape(Circle())
                 .matchedTransitionSource(id: "\(attendee.id.uuidString)", in: namespace)
+        }
+        .contentShape(.contextMenuPreview, .circle)
+        .contextMenu {
+            ControlGroup {
+                if let phone = attendee.phone?.replacingOccurrences(of: " ", with: "") {
+                    Button("Call", systemImage: "phone") {
+                        openURL(URL(string: "tel:\(phone)")!)
+                    }
+                    
+                    Button("Message", systemImage: "message") {
+                        openURL(URL(string: "sms:\(phone)")!)
+                    }
+                }
+                if let email = attendee.email {
+                    Button("Email", systemImage: "envelope") {
+                        openURL(URL(string: "mailto:\(email)")!)
+                    }
+                }
+            }
+            Button("Delete", systemImage: "trash", role: .destructive) {
+                showDeleteConfirmation = true
+            }
+        }
+        .alert("Are you sure you want to delete \(attendee.name)?", isPresented: $showDeleteConfirmation) {
+            Button("Delete", role: .destructive) {
+                modelContext.delete(attendee)
+            }
+            
+            Button("Cancel") {
+                showDeleteConfirmation = false
+            }
+        } message: {
+            Text("This action cannot be undone.")
         }
     }
 }
